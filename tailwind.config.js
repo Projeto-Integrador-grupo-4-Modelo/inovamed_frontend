18bb235ada893598ce1a196b95b2df2061b816bc
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2025-01-31 18:58:20 -0300

    Adicionando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-<<<<<<< HEAD
-  content: [
-    "./index.html",
-"./src/**/*.{js,ts,jsx,tsx}",
-  ],
-=======
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
->>>>>>> ee3f85137a65a5e1130164d093bae7c751f7b7f2
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        teal: "#0D9488",
-        tealDark: "#0D9389",
-        blue: "#1C74C8",
-        blueDark: "#2464E9",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
