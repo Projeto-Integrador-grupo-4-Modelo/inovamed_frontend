8c555bedbee8263bc1f26232279aa935b49ef230
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-02-02 11:27:01 -0300

    criado as rotas e ajustado

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,18 @@
 import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import Carrossel from "./Carrossel";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleAcessarSistema = () => {
+    navigate("/login");
+  };
+
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
-        <nav className="bg-white shadow-sm">
+      <div className="min-h-screen ">
+        <nav className="bg-white shadow-md rounded-lg hover:shadow-xl transition-shadow cursor-pointer">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
             <div className="flex justify-between items-center">
               <div className="flex items-center space-x-2">
@@ -15,10 +22,13 @@ function Home() {
               </div>
 
               <div className="flex items-center space-x-4">
-                <button className="text-teal-600 px-4 py-2 rounded-lg hover:text-teal-800 transition-colors">
+                <button className="text-teal-600 px-4 py-2 rounded-lg hover:text-teal-800 transition-colors transform hover:scale-105 hover:shadow-md">
                   <span>Sobre</span>
                 </button>
-                <button className="bg-teal-600 text-white px-6 py-2 rounded-lg hover:bg-teal-700 transition-colors flex items-center space-x-2">
+                <button
+                  onClick={handleAcessarSistema} // Evento de click
+                  className="bg-teal-600 text-white px-6 py-2 rounded-lg hover:bg-teal-700 transition-colors transform hover:scale-105 hover:shadow-lg flex items-center space-x-2"
+                >
                   <span>Acessar Sistema</span>
                   <ArrowRight className="h-4 w-4" />
                 </button>
